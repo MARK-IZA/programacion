3a96435635c58bf8a550e5a41992b21f7bc154bc
MarkAntoniIzaM <markantoniiza@gmail.com>
2022-10-19 10:21:38 +0200

    subida 19/10

---
+++ src/sesion1710ejer/Ej3Buclewhiledo.java
@@ -0,0 +1,13 @@
+package sesion1710ejer;
+
+public class Ej3Buclewhiledo {
+    public static void main(String[] args) {
+        System.out.println("Múltiplos de 5");
+        int i=0;
+
+        do {
+            System.out.println(i);
+            i+=5;
+        } while(i <= 100);
+    }
+}
---
+++ src/sesion1710ejer/Eje1Buclewhile.java
@@ -0,0 +1,13 @@
+package sesion1710ejer;
+
+public class Eje1Buclewhile {
+    public static void main(String[] args) {
+        System.out.println("Múltiplos de 5");
+        int i = 0;
+
+        while (i <= 100) {
+            System.out.println(i);
+            i += 5;
+        }
+    }
+}
